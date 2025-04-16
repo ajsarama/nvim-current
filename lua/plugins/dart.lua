c707afa3e41f3c88c4de3afc88afb9449c4365b1
Andrew Sarama <ajsarama.cs@gmail.com>
2025-04-16 17:30:41 -0400

    simplifying config

--- lua/plugins/dart.lua
+++ lua/plugins/dart.lua
@@ -1,1 +1,17 @@
-return { "dart-lang/dart-vim-plugin" }
+local dart= { "dart-lang/dart-vim-plugin" }
+
+local flutter =
+{
+	'nvim-flutter/flutter-tools.nvim',
+	lazy = false,
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+	},
+	opts = {
+		debugger = {
+			enabled = true
+		}
+	},
+}
+
+return { dart, flutter }
--- lua/plugins/oil.lua
+++
@@ -1,6 +0,0 @@
-return {
-	'stevearc/oil.nvim',
-	opts = {},
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	lazy = false,
-}
