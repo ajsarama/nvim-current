2dfee2136c42ff7b454aeb937faedc47e4b19d59
Andrew Sarama <ajsarama.cs@gmail.com>
2025-04-29 16:55:07 -0400

    removed dark theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,14 @@
--- Lua
-local auto_dark = {
-	"f-person/auto-dark-mode.nvim",
-	dependencies = { "rose-pine/neovim" },
-	priority = 1000,
-	opts = {
-		set_dark_mode = function()
-			vim.o.background = 'dark'
-			vim.cmd.colorscheme('rose-pine')
-		end,
-		set_light_mode = function()
-			vim.o.background = 'light'
-			vim.cmd.colorscheme('rose-pine')
-		end,
-	}
-}
-
--- lua/plugins/rose-pine.lua
 local rose = {
 	"rose-pine/neovim",
 	priority = 1000,
 	name = "rose-pine",
 	config = function(opts)
 		require("rose-pine").setup()
+		vim.o.background = 'light'
+		vim.cmd.colorscheme('rose-pine')
 	end
 }
 
 
 
-return { auto_dark, rose }
+return { rose }
