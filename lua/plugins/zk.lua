729cc97da97031674e253e4208a082940ce3d1b1
Andrew Sarama <ajsarama.cs@gmail.com>
2025-05-04 18:10:34 -0400

    add keymaps for zk

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,8 +1,12 @@
 return {
-  "zk-org/zk-nvim",
-  config = function()
-    require("zk").setup({
-		picker = "fzf_lua",
-    })
-  end
+	"zk-org/zk-nvim",
+	config = function()
+		require("zk").setup({
+			picker = "fzf_lua",
+		})
+	end,
+	init = function()
+		vim.keymap.set("n", "<leader>zn", "<cmd>ZkNew<cr>")
+		vim.keymap.set("n", "<leader>ze", "<cmd>ZkNotes<cr>")
+	end,
 }
